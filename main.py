7a82ce252f7d42777200293e26dbdc227861585b
Angelo B. <81601652+abarbierif@users.noreply.github.com>
2024-11-18 21:56:46 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
-import sys
-
-from tictactoegui import TicTacToeGui
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    TicTacToeWindow = TicTacToeGui()
-    app.exec_()
